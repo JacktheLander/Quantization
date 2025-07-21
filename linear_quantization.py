9307642342f1abf29a06ad8074476874032007ad
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2025-07-21 10:54:44 -0700

    Update linear_quantization.py

--- linear_quantization.py
+++ linear_quantization.py
@@ -9,8 +9,7 @@ import torch
 ## Linear Quantization uses a Scale and Zero Point ##
 
 # r = s(q-z)
-def linear_q_with_scale_and_zero_point(
-    tensor, scale, zero_point, dtype = torch.int8):
+def linear_q_with_scale_and_zero_point(tensor, scale, zero_point, dtype = torch.int8):
 
     scaled_and_shifted_tensor = tensor / scale + zero_point
 
@@ -35,8 +34,7 @@ test_tensor=torch.tensor(
 scale = 3.5
 zero_point = -70
 
-quantized_tensor = linear_q_with_scale_and_zero_point(
-    test_tensor, scale, zero_point)
+quantized_tensor = linear_q_with_scale_and_zero_point(test_tensor, scale, zero_point)
 
 print(quantized_tensor)
 
@@ -63,6 +61,15 @@ print(dequantized_tensor)
 
 
 ## Quantization Error Calculation ##
+
+def plot_matrix(tensor, ax, title, vmin=0, vmax=1, cmap=None):
+    """
+    Plot a heatmap of tensors using seaborn
+    """
+    sns.heatmap(tensor.cpu().numpy(), ax=ax, vmin=vmin, vmax=vmax, cmap=cmap, annot=True, fmt=".2f", cbar=False)
+    ax.set_title(title)
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
 
 def plot_quantization_errors(original_tensor, quantized_tensor, dequantized_tensor, dtype = torch.int8, n_bits = 8):
     """
