38ab153f4b489b10fef039a72930df5dae6df000
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2025-07-21 10:11:19 -0700

    Update linear_quantization.py

--- linear_quantization.py
+++ linear_quantization.py
@@ -1,4 +1,12 @@
+### Quantizing and Dequantizing a Tensor with Torch ###
+
 import torch
+import seaborn as sns
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import torch
+
+## Quantization with a Random Scale and Zero Point ##
 
 def linear_q_with_scale_and_zero_point(
     tensor, scale, zero_point, dtype = torch.int8):
@@ -29,4 +37,59 @@ zero_point = -70
 quantized_tensor = linear_q_with_scale_and_zero_point(
     test_tensor, scale, zero_point)
 
-quantized_tensor
+print(quantized_tensor)
+
+
+## Dequantization with a Random Scale and Zero Point ##
+
+dequantized_tensor = scale * (quantized_tensor.float() - zero_point)
+
+# this was the original tensor
+# [[191.6, -13.5, 728.6],
+#  [92.14, 295.5,  -184],
+#  [0,     684.6, 245.5]]
+
+print(dequantized_tensor)
+
+### without casting to float
+scale * (quantized_tensor - zero_point)
+
+def linear_dequantization(quantized_tensor, scale, zero_point):
+    return scale * (quantized_tensor.float() - zero_point)
+
+dequantized_tensor = linear_dequantization(quantized_tensor, scale, zero_point)
+print(dequantized_tensor)
+
+
+## Quantization Error Calculation ##
+
+def plot_quantization_errors(original_tensor, quantized_tensor, dequantized_tensor, dtype = torch.int8, n_bits = 8):
+    """
+    A method that plots 4 matrices, the original tensor, the quantized tensor
+    the de-quantized tensor and the error tensor.
+    """
+    # Get a figure of 4 plots
+    fig, axes = plt.subplots(1, 4, figsize=(15, 4))
+
+    # Plot the first matrix
+    plot_matrix(original_tensor, axes[0], 'Original Tensor', cmap=ListedColormap(['white']))
+
+    # Get the quantization range and plot the quantized tensor
+    q_min, q_max = torch.iinfo(dtype).min, torch.iinfo(dtype).max
+    plot_matrix(quantized_tensor, axes[1], f'{n_bits}-bit Linear Quantized Tensor', vmin=q_min, vmax=q_max, cmap='coolwarm')
+
+    # Plot the de-quantized tensors
+    plot_matrix(dequantized_tensor, axes[2], 'Dequantized Tensor', cmap='coolwarm')
+
+    # Get the quantization errors
+    q_error_tensor = abs(original_tensor - dequantized_tensor)
+    plot_matrix(q_error_tensor, axes[3], 'Quantization Error Tensor', cmap=ListedColormap(['white']))
+
+    fig.tight_layout()
+    plt.show()
+
+plot_quantization_errors(test_tensor, quantized_tensor, dequantized_tensor)
+
+print("Error:", dequantized_tensor - test_tensor)
+print("MSE:", (dequantized_tensor - test_tensor).square())
+print("RMSE:", (dequantized_tensor - test_tensor).square().mean())
