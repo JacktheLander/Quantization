17d48712ab4e92e4e1b919304f006e4622debdbe
Jack Landers <121060192+JacktheLander@users.noreply.github.com>
2025-07-22 14:59:59 -0700

    Update quantize_layers.py

--- quantize_layers.py
+++ quantize_layers.py
@@ -46,28 +46,21 @@ print(model_2)
 
 
 ## Here we quantize the weights as well reducing model size ##
-def replace_linear_with_target_and_quantize(module, 
-                               target_class, module_name_to_exclude):
+def replace_linear_with_target_and_quantize(module, target_class, module_name_to_exclude):
     for name, child in module.named_children():
-        if isinstance(child, nn.Linear) and not \
-        any([x == name for x in module_name_to_exclude]):
+        if isinstance(child, nn.Linear) and not any([x == name for x in module_name_to_exclude]):
             old_bias = child.bias
             old_weight = child.weight
 
-            new_module = target_class(child.in_features, 
-                                      child.out_features, 
-                                      old_bias is not None, 
-                                      child.weight.dtype)
+            new_module = target_class(child.in_features, child.out_features, old_bias is not None, child.weight.dtype)
             setattr(module, name, new_module)
-
             getattr(module, name).quantize(old_weight)
             
             if old_bias is not None:
               getattr(module, name).bias = old_bias
         else:
             # Recursively call the function for nested modules
-            replace_linear_with_target_and_quantize(child, 
-                     target_class, module_name_to_exclude)
+            replace_linear_with_target_and_quantize(child, target_class, module_name_to_exclude)
 
 model_3 = DummyModel()
 replace_linear_with_target_and_quantize(model_3, W8A16LinearLayer, ["lm_head"])
